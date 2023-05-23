09b71091113146e94b94fc482f6860eb1c6f9a90
Pedro Souza <phmartinsdesouza@gmail.com>
2023-05-23 14:58:03 -0300

    Challenge is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :tasks
   # Defines the root path route ("/")
   # root "articles#index"
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :update_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
